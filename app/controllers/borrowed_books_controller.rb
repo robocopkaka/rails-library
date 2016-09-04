55cc6696dbf5b48fc0e94768519b3c9df1ed4f34
robocopkaka <robocopkaka@gmail.com>
2016-09-04 01:01:46 +0100

    Can now return borrowed books for a particular user, and for all users, and can use them in views

--- app/controllers/borrowed_books_controller.rb
+++ app/controllers/borrowed_books_controller.rb
@@ -5,11 +5,16 @@
 class BorrowedBooksController < ApplicationController
 	#return books borrowed by a particular user
 	def user_borrowed_books
-		@books = current_user.books
+		@books = BorrowedBook.includes(:book, :user).where("user_id = ?", current_user.id).all
 	end
 
 	#return all borrowed books and the user that borrowed them
 	def all_borrowed_books
-		@books = Book.joins(:borrowed_books, :users).uniq
+		#use includes to join the borrowed_books, books, and users tables
+		#includes takes in the model names in singular forms
+		# I think joins uses the plural forms for the table names passes as parameters
+		# to access any element of the books/ users tables in the views, do:
+		# @iteration_variable.book.property or @iteration_variable.user.property 
+		@books = BorrowedBook.includes(:book, :user)
 	end
 end
