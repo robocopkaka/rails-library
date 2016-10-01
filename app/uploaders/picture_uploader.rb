457e298a0bd515a2214eb2ed7c95209a1537e9e1
robocopkaka <robocopkaka@gmail.com>
2016-10-01 15:24:00 +0100

    Added carrierwave config for production environment

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -16,8 +20,8 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fill: [400, 400]
-  process resize_to_fit: [400, 400]
+  process resize_to_fill: [500, 600]
+  process resize_to_fit: [500, 600]
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
